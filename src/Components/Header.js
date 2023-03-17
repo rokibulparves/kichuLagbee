56dfb98a387490b918a1140c8c1c3c92f9697c89
rokibulparves <hmrokibul@gmail.com>
2023-03-18 00:53:04 +0600

    header 2nd part

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <header className='header-top-strip py-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <p className='text-white mb-0'>Free shipping over $100 & free returns</p>
+                        </div>
+                        <div className='col-6'>
+                            <p className='text-end text-white mb-0'>
+                                Hotline: <a href='tel:+8801795722318'>+8801795722318</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </header>
+
+            <header className='header-upper py-3'>
+                <div className='container-xxl'>
+                    <div className='row py-2'>
+                        <div className='col-2'>
+                            <h3>
+                                <Link className='text-white logo'>KichuLagbe?</Link>
+                            </h3>
+                        </div>
+                        <div className='col-5'>
+                            <div class="input-group">
+                                <input 
+                                type="text" 
+                                class="form-control" 
+                                placeholder="Enter Your Items" 
+                                aria-label="Enter Your Items" 
+                                aria-describedby="basic-addon2" 
+                                />
+                    
+                                <span class="input-group-text" id="basic-addon2">
+                                    <BsSearch />
+                                </span>
+                            </div>
+
+                        </div>
+                        <div className='col-6'>
+
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 };
 
